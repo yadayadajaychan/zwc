c98d7ad00d07160c41f024190f7f06c0dc5a224d
Ethan Cheng <ethanrc0528@gmail.com>
2023-08-20 12:43:50 -0700

    reorganize tests and add tests for no-message

--- test-data/test.sh
+++ test-data/test.sh
@@ -6,7 +6,7 @@ if [ "$0" != "$SCRIPT_PATH" ] && [ "$SCRIPT_PATH" != "" ]; then
 fi
 
 # vanilla encode
-for dir in */
+for dir in vanilla/*/
 do
 	source ${dir}/parameters
 	go run ../main/main.go encode -v -m ${dir}/*.mesg -d ${dir}/*.data -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
@@ -17,4 +17,55 @@ do
 	fi
 done
 
-echo test.sh: tests passed
+## data from stdin
+
+for dir in vanilla/*/
+do
+	source ${dir}/parameters
+	cat ${dir}/*.data | go run ../main/main.go encode -v -m ${dir}/*.mesg -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
+	if [ $? -ne 0 ]
+	then
+		echo error during vanilla encode with data from stdin
+		exit 1
+	fi
+done
+
+## message from stdin
+
+for dir in vanilla/*/
+do
+	source ${dir}/parameters
+	cat ${dir}/*.mesg | go run ../main/main.go encode -v -d ${dir}/*.data -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
+	if [ $? -ne 0 ]
+	then
+		echo error during vanilla encode with message from stdin
+		exit 1
+	fi
+done
+
+# no-message encode
+for dir in no-message/*/
+do
+	source ${dir}/parameters
+	go run ../main/main.go encode -vn -d ${dir}/*.data -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
+	if [ $? -ne 0 ]
+	then
+		echo error during no-message encode
+		exit 1
+	fi
+done
+
+## data from stdin
+
+for dir in no-message/*/
+do
+	source ${dir}/parameters
+	cat ${dir}/*.data | go run ../main/main.go encode -vn -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
+	if [ $? -ne 0 ]
+	then
+		echo error during no-message encode with data from stdin
+		exit 1
+	fi
+done
+
+echo test.sh: all tests passed
