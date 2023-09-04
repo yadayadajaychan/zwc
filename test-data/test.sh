3b609684d71bb14350f047504a31130d92356443
Ethan Cheng <ethanrc0528@gmail.com>
2023-09-04 11:37:09 -0700

    refactor test.sh

--- test-data/test.sh
+++ test-data/test.sh
@@ -2,120 +2,54 @@
 
 SCRIPT_PATH="${0%/*}"
 if [ "$0" != "$SCRIPT_PATH" ] && [ "$SCRIPT_PATH" != "" ]; then
-    cd $SCRIPT_PATH
+	cd $SCRIPT_PATH
 fi
 
 export GOCOVERDIR="test-coverage"
 rm -r test-coverage
 mkdir test-coverage
 
+# exit if any commands fail
+set -e
+
 go build -cover -o zwc ../main/main.go
 
-# vanilla encode
 for dir in vanilla/*/
 do
 	source ${dir}/parameters
-	./zwc encode -v -m ${dir}/*.mesg -d ${dir}/*.data -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
-	if [ $? -ne 0 ]
-	then
-		echo error during vanilla encode
-		exit 1
-	fi
-done
 
-## data from stdin
-for dir in vanilla/*/
-do
-	source ${dir}/parameters
-	cat ${dir}/*.data | ./zwc encode -v -m ${dir}/*.mesg -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
-	if [ $? -ne 0 ]
-	then
-		echo error during vanilla encode with data from stdin
-		exit 1
-	fi
-done
+	# encode
+	./zwc encode -m ${dir}/*.mesg -d ${dir}/*.data -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
 
-## message from stdin
-for dir in vanilla/*/
-do
-	source ${dir}/parameters
-	cat ${dir}/*.mesg | ./zwc encode -v -d ${dir}/*.data -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
-	if [ $? -ne 0 ]
-	then
-		echo error during vanilla encode with message from stdin
-		exit 1
-	fi
-done
+	## data from stdin
+	cat ${dir}/*.data | ./zwc encode -m ${dir}/*.mesg -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
 
-# no-message encode
-for dir in no-message/*/
-do
-	source ${dir}/parameters
-	./zwc encode -vn -d ${dir}/*.data -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
-	if [ $? -ne 0 ]
-	then
-		echo error during no-message encode
-		exit 1
-	fi
-done
+	## message from stdin
+	cat ${dir}/*.mesg | ./zwc encode -d ${dir}/*.data -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
 
-## data from stdin
-for dir in no-message/*/
-do
-	source ${dir}/parameters
-	cat ${dir}/*.data | ./zwc encode -vn -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
-	if [ $? -ne 0 ]
-	then
-		echo error during no-message encode with data from stdin
-		exit 1
-	fi
-done
-
-# vanilla decode
-for dir in vanilla/*/
-do
+	# decode
 	./zwc decode -t ${dir}/*.txt | diff -q - ${dir}/*.data
-	if [ $? -ne 0 ]
-	then
-		echo error during vanilla decode
-		exit 1
-	fi
-done
 
-## text from stdin
-for dir in vanilla/*/
-do
+	## text from stdin
 	cat ${dir}/*.txt | ./zwc decode | diff -q - ${dir}/*.data
-	if [ $? -ne 0 ]
-	then
-		echo error during vanilla decode with text from stdin
-		exit 1
-	fi
 done
 
-
-# no-message decode
 for dir in no-message/*/
 do
+	source ${dir}/parameters
+
+	# no-message encode
+	./zwc encode -n -d ${dir}/*.data -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
+
+	## data from stdin
+	cat ${dir}/*.data | ./zwc encode -n -c $CHECKSUM -e $ENCODING | diff -q - ${dir}/*.txt
+
+	# no-message decode
 	./zwc decode -t ${dir}/*.txt | diff -q - ${dir}/*.data
-	if [ $? -ne 0 ]
-	then
-		echo error during no-message decode
-		exit 1
-	fi
-done
 
-## text from stdin
-for dir in no-message/*/
-do
+	## text froms stdin
 	cat ${dir}/*.txt | ./zwc decode | diff -q - ${dir}/*.data
-	if [ $? -ne 0 ]
-	then
-		echo error during no-message decode with text from stdin
-		exit 1
-	fi
 done
-
 
 rm zwc
 
